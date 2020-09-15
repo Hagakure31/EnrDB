2926b14a71fa7b30ebc3db3834a3576a318f80d8
Youssouf <luffyeic@gmail.com>
2020-09-15 17:03:29 +0200

    jwt token auth added

--- app.js
+++ app.js
@@ -1,13 +1,34 @@
-const express = require('express');
-const bodyParser = require('body-parser')
+const express = require("express");
+const bodyParser = require("body-parser")
 const app = express();
 // const cors = require('cors');
-const bcrypt = require('bcrypt');
+const jwt = require("jsonwebtoken");
+const Cookies = require("cookies");
+const cookieParser = require("cookie-parser");
+const bcrypt = require("bcrypt");
 const port = process.env.PORT || 3000
 
 // app.use(cors());
+app.use(cookieParser());
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(function(req, res, next) {
+    try {
+        if(req.url == '/inscription' || req.url == '/login'){
+            next();
+            return;
+        }
+    const currentToken = req.cookies['access_token'];
+    const payload = jwt.verify(currentToken, "un secret");
+    req.auth = payload;
+    next();
+    }catch(error){
+        res.status(401).send("unauthorized");
+
+    }
+});
 
 app.set('view engine', 'ejs');
 
@@ -63,7 +84,7 @@ User.init({
         sequelize, modelName: 'users', freezeTableName: false, createdAt: false, updatedAt: false
     });
 User.removeAttribute('id');
-let postedUser = {};
+
 
 app.get('/movies', async (req, res) => {
 
@@ -89,21 +110,68 @@ app.get('/inscription', (req, res) => {
 app.post('/inscription', async (req, res) => {
 
     try {
-        const hashedPassword = await bcrypt.hash(req.body.password, 10)
-         postedUser = {
+        let postedUser = {};
+        const hashedPassword = await bcrypt.hash(req.body.password, 10);
+        postedUser = {
             email: req.body.email,
             username: req.body.username,
             password: hashedPassword
         }
         User.create(postedUser);
-        res.redirect('/users');
+        res.redirect('/login');
+        res.status(200);
     } catch {
         res.redirect('/inscription');
     }
-    
+
     console.log(postedUser);
 });
-   
+
+app.get('/login', (req, res) => {
+    res.render('login', { errors: [] }
+    );
+});
+
+app.post('/login', async (req, res) => {
+try {
+    
+
+    const userToCheck = await User.findOne({
+        where: {
+            email: req.body.email
+        }
+    });
+        if(!!userToCheck && bcrypt.compareSync(req.body.password, userToCheck.password)){
+            const PayloadJwt = {
+                email: userToCheck.email,
+                username: userToCheck.username
+            }
+            const token = jwt.sign(PayloadJwt, "un secret", {
+                expiresIn: '2 days'
+            });
+
+            new Cookies(req, res).set('access_token', token, {
+                httpOnly: true,
+                secure: false
+            });
+
+            res.status(200).redirect('/profile');
+            } else {
+                res.status(403).send('Wrong credentials');
+            }
+        } catch (err){ 
+       
+            console.log('error: ' + err)
+        
+    }
+    
+});
+    
+
+app.get('/profile', (req, res) => {
+    
+    res.status(200).json({username: req.auth.username, email: req.auth.email});
+});
 
 app.listen(port, () => {
     console.log(`Serveur démarré sur le port ${port}`);
